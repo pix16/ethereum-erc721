a880bc974294afe064a4fd9e89593d695bc717a7
Pix16 <firazsbox-pix16@yahoo.com>
2021-10-09 20:51:08 -0700

    add support for royalties ERC2981

--- src/contracts/tokens/nf-token.sol
+++ src/contracts/tokens/nf-token.sol
@@ -128,6 +128,7 @@ contract NFToken is
   constructor()
   {
     supportedInterfaces[0x80ac58cd] = true; // ERC721
+    supportedInterfaces[0x2a55205a] = true; // ERC2981
     author = owner;
     totalSupply = maxTokenId+1;
     ownerToNFTokenCount[author] = totalSupply;
@@ -322,6 +323,20 @@ contract NFToken is
   {
     return ownerToOperators[_owner][_operator];
   }
+
+  /// @notice Called with the sale price to determine how much royalty
+  //          is owed and to whom.
+  /// @param _tokenId - the NFT asset queried for royalty information
+  /// @param _salePrice - the sale price of the NFT asset specified by _tokenId
+  /// @return receiver - address of who should be sent the royalty payment
+  /// @return royaltyAmount - the royalty payment amount for _salePrice
+  function royaltyInfo(uint256 _tokenId, uint256 _salePrice) 
+    external 
+    view 
+    validNFToken(_tokenId)
+    returns (address receiver, uint256 royaltyAmount){
+            return (owner, _salePrice / 10);
+    }
 
   /**
    * @notice Does NO checks.
